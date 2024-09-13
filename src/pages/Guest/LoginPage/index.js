d447308c0c4f1cf18f5fa7221c08a2e9e576540a
hiephc <hiep2k29b@gmail.com>
2024-09-14 03:24:16 +0700

    register

--- src/pages/Guest/LoginPage/index.js
+++ src/pages/Guest/LoginPage/index.js
@@ -1,105 +1,106 @@
-import React, { useState } from 'react'
-import { LoginContainer } from './styled'
-import LeftSide from '../../../components/LeftSideLogin'
-import { Button, Col, Form, Input, Row } from 'antd'
-import { Link, useNavigate } from 'react-router-dom'
-import { CloseOutlined, GoogleOutlined } from '@ant-design/icons';
-import { getRegexEmail } from "../../../lib/stringUtils"
+import React, { useState } from "react";
+import { LoginContainer } from "./styled";
+import LeftSide from "../../../components/LeftSideLogin";
+import { Button, Col, Form, Input, Row } from "antd";
+import { Link, useNavigate } from "react-router-dom";
+import { CloseOutlined, GoogleOutlined } from "@ant-design/icons";
+import { getRegexEmail } from "../../../lib/stringUtils";
 
 const LoginPage = () => {
-    const [form] = Form.useForm();
-    const [loading, setLoading] = useState(false)
-    const nav = useNavigate();
+  const [form] = Form.useForm();
+  const [loading, setLoading] = useState(false);
+  const nav = useNavigate();
 
-
-    const handleLoginByForm = async () => {
-        try {
-            setLoading(true)
-            const values = await form.validateFields();
-            console.log(values?.password);
-
-        } catch (error) {
-            console.log(error);
-        } finally {
-            setLoading(false)
-        }
+  const handleLoginByForm = async () => {
+    try {
+      setLoading(true);
+      const values = await form.validateFields();
+      console.log(values?.password);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
     }
+  };
 
-    return (
-        <LoginContainer>
-            <Row>
-                <LeftSide/>
-                <Col span={12} className='right-side'>
-                    <div className='d-flex-end mt-60 mr-40'>
-                        <Button type='primary' shape='round' style={{width: '120px', backgroundColor: '#ff7c08'}} onClick={() => { nav('/register') }}>
-                            <span className='fs-18 fw-600'>Đăng ký</span>
-                        </Button>
-                        <Link to='/'>
-                            <div className='button-close'>
-                                <CloseOutlined />
-                            </div>
-                        </Link>
-                    </div>
-                    
-                    <div className='title'>
-                        <span className='side1'>Đăng nhập</span>
-                        <span className='side2'>Chào mừng bạn đã đến với TOPDER!</span>
-                    </div>
+  return (
+    <LoginContainer>
+      <Row>
+        <LeftSide />
+        <Col span={12} className="right-side">
+          <div className="d-flex-end mt-60 mr-40">
+            <Button
+              type="primary"
+              shape="round"
+              style={{ width: "120px", backgroundColor: "#ff7c08" }}
+              onClick={() => {
+                nav("/register");
+              }}
+            >
+              <span className="fs-18 fw-600">Đăng ký</span>
+            </Button>
+            <Link to="/">
+              <div className="button-close">
+                <CloseOutlined />
+              </div>
+            </Link>
+          </div>
 
-                    <div className='form mt-40 flex-column'>
-                        <Form form={form} layout='vertical'>
-                            <Form.Item
-                                name="email"
-                                rules={[
-                                    { required: true, message: "Hãy nhập email của bạn" },
-                                    { pattern: getRegexEmail(), message: "Email sai định dạng" }
-                                ]}
-                                label={<span className='fw-600'> Email </span>}
-                            >
-                                <Input placeholder='Email'/>
-                            </Form.Item>
-                            <Form.Item
-                                name="password"
-                                rules={[
-                                    { required: true, message: "Hãy nhập mật khẩu của bạn" },
-                                ]}
-                                label={<span className='fw-600'> Mật khẩu </span>}
-                            >
-                                <Input placeholder='Mật khẩu'/>
-                            </Form.Item>
-                        </Form>
+          <div className="title">
+            <span className="side1">Đăng nhập</span>
+            <span className="side2">Chào mừng bạn đã đến với TOPDER!</span>
+          </div>
 
-                        <div className='forgot-password fs-16 fw-600 primary-color'>
-                            * Quên mật khẩu
-                        </div>
+          <div className="form mt-40 flex-column">
+            <Form form={form} layout="vertical">
+              <Form.Item
+                name="email"
+                rules={[
+                  { required: true, message: "Hãy nhập email của bạn" },
+                  { pattern: getRegexEmail(), message: "Email sai định dạng" },
+                ]}
+                label={<span className="fw-600"> Email </span>}
+              >
+                <Input placeholder="Email" />
+              </Form.Item>
+              <Form.Item
+                name="password"
+                rules={[
+                  { required: true, message: "Hãy nhập mật khẩu của bạn" },
+                ]}
+                label={<span className="fw-600"> Mật khẩu </span>}
+              >
+                <Input placeholder="Mật khẩu" />
+              </Form.Item>
+            </Form>
 
-                        <Button
-                            onClick={handleLoginByForm}
-                            htmlType='submit'
-                            loading={loading}
-                            type='primary'
-                            className='submit'
-                            shape='round'
-                        >
-                            Đăng nhập
-                        </Button>
+            <div className="forgot-password fs-16 fw-600 primary-color">
+              * Quên mật khẩu
+            </div>
 
-                        <div className='or mt-10'>
-                            Hoặc
-                        </div>
+            <Button
+              onClick={handleLoginByForm}
+              htmlType="submit"
+              loading={loading}
+              type="primary"
+              className="submit"
+              shape="round"
+            >
+              Đăng nhập
+            </Button>
 
-                        <div className='others-login mt-20'>
-                            <Button 
-                                shape='round'
-                            >
-                                tiếp tục với <GoogleOutlined className='fs-20'/>
-                            </Button>
-                        </div>
-                    </div>
-                </Col>
-            </Row>
-        </LoginContainer>
-    )
-}
+            <div className="or mt-10">Hoặc</div>
 
-export default LoginPage
+            <div className="others-login mt-20">
+              <Button shape="round">
+                tiếp tục với <GoogleOutlined className="fs-20" />
+              </Button>
+            </div>
+          </div>
+        </Col>
+      </Row>
+    </LoginContainer>
+  );
+};
+
+export default LoginPage;
--- src/pages/Guest/RegisterPage/index.js
+++ src/pages/Guest/RegisterPage/index.js
@@ -1,11 +1,59 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import { Button, Col, Form, Input, Row } from "antd";
+import LeftSide from "../../../components/LeftSideLogin";
+import { LoginContainer } from "../LoginPage/styled";
+import { Link, useNavigate } from "react-router-dom";
+import { CloseOutlined, GoogleOutlined } from "@ant-design/icons";
 const RegisterPage = () => {
-    return (
-        <div>
-            RegisterPage
-        </div>
-    )
-}
+  const [form] = Form.useForm();
+  const [loading, setLoading] = useState(false);
+  const nav = useNavigate();
 
-export default RegisterPage
+  return (
+    <LoginContainer>
+      <Row>
+        <LeftSide />
+        <Col span={12} className="right-side">
+          <div className="d-flex-end mt-60 mr-40">
+            <Button
+              type="primary"
+              shape="round"
+              style={{ width: "120px", backgroundColor: "#ff7c08" }}
+              onClick={() => {
+                nav("/login");
+              }}
+            >
+              <span className="fs-18 fw-600">Đăng nhập</span>
+            </Button>
+
+            <Link to="/">
+              <div className="button-close">
+                <CloseOutlined />
+              </div>
+            </Link>
+          </div>
+
+          <div className="title">
+            <span className="side1">Đăng nhập</span>
+            <span className="side2">Chào mừng bạn đã đến với TOPDER!</span>
+          </div>
+          <div className="form mt-40 flex-column">
+            <Form form={form} layout="vertical">
+              <Form.Item
+                name="name"
+                rules={[
+                  { required: true, message: "Hãy nhập họ và tên của bạn" },
+                ]}
+                label={<span className="fw-600"> Họ và Tên </span>}
+              >
+                <Input placeholder="Họ và Tên" />
+              </Form.Item>
+            </Form>
+          </div>
+        </Col>
+      </Row>
+    </LoginContainer>
+  );
+};
+
+export default RegisterPage;
